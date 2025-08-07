2c9b3f01b85c188b7317f47ebaaa92d67efde971
DOMOSH85 <157604004+DOMOSH85@users.noreply.github.com>
2025-08-08 01:16:26 +0300

    greenlands

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { BrowserRouter } from 'react-router-dom';
 
 import Navbar from './components/Navbar';
 import SignInModal from './components/SignInModal';
@@ -15,27 +14,20 @@ function App() {
   const [showSignIn, setShowSignIn] = useState(false);
   const [showSignUp, setShowSignUp] = useState(false);
 
-  const handleShowSignIn = () => {
-    setShowSignUp(false);
-    setShowSignIn(true);
-  };
-
   return (
-    <BrowserRouter>
-      <div className="min-h-screen flex flex-col bg-gradient-to-b from-green-50 to-green-100">
-        <Navbar onSignIn={() => setShowSignIn(true)} onSignUp={() => setShowSignUp(true)} />
-        <main className="flex-1 px-4 py-12 max-w-5xl mx-auto text-center">
-          <DescriptionSection />
-          <ServiceCards />
-          <CTASection onSignUp={() => setShowSignUp(true)} />
-          <TestimonialSection />
-          <NewsletterSection />
-        </main>
-        <Footer />
-        <SignInModal open={showSignIn} onClose={() => setShowSignIn(false)} />
-        <SignUpModal open={showSignUp} onClose={() => setShowSignUp(false)} onShowSignIn={handleShowSignIn} />
-      </div>
-    </BrowserRouter>
+    <div className="min-h-screen flex flex-col bg-gradient-to-b from-green-50 to-green-100">
+      <Navbar onSignIn={() => setShowSignIn(true)} onSignUp={() => setShowSignUp(true)} />
+      <main className="flex-1 px-4 py-12 max-w-5xl mx-auto text-center">
+        <DescriptionSection />
+        <ServiceCards />
+        <CTASection onSignUp={() => setShowSignUp(true)} />
+        <TestimonialSection />
+        <NewsletterSection />
+      </main>
+      <Footer />
+      <SignInModal open={showSignIn} onClose={() => setShowSignIn(false)} />
+      <SignUpModal open={showSignUp} onClose={() => setShowSignUp(false)} />
+    </div>
   );
 }
 
